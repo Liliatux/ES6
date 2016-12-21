3946dbdd56740506242d7d51fd428f126d5a621d
Lilia <geertszelia@gmail.com>
2016-12-21 15:08:49 +0100

    Etape 3 fini

--- public/app.js
+++ public/app.js
@@ -3,22 +3,43 @@
 //clique bouton affiche
 var tab = [];
 
-var es6 = function es6() {
+//ajout d'un nom
+document.getElementById('addName').addEventListener('click', function () {
 	var inputVal = document.getElementById('nom').value;
+
 	//mettre en maj la première lettre des données de l'input
 	var majName = inputVal.charAt(0).toUpperCase();
 	var name = majName + inputVal.slice(1);
+
 	//Si champ vide "Bonjour Père Noël !"
-	if (inputVal === "") {
+	if (!name) {
 		document.getElementById('message').innerHTML = "Bonjour Père Noël !";
 	}
 	// Si input rempli "Bonjour {nom saisi} !"
 	else {
-			document.getElementById('message').innerHTML = 'Bonjour ' + inputVal;
+			document.getElementById('message').innerHTML = 'Bonjour ' + name;
 			//les données de l'input modifiées s'affichent en liste
 			document.getElementById('list').appendChild(document.createElement('li')).innerHTML = name;
 			//les données de l'input modifiées s'ajoutent au tableau
 			tab.push(name);
-			console.log(tab);
 		}
-};
+});
+
+//cherche un nom
+document.getElementById('searchName').addEventListener('click', function () {
+	var filterTab = [];
+	var inputSearch = document.getElementById('search').value;
+	var searchLower = inputSearch.toLowerCase();
+	//comparer les données entrées dans l'input search avec le tableau
+	for (var i = 0; i < tab.length; i++) {
+		var element = tab[i];
+		var elementLower = element.toLowerCase();
+		var filter = elementLower.indexOf(searchLower);
+		//affichage dans result des noms du tableau "commençant" par la chaîne de caractères saisie dans inputSearch
+		if (filter === 0) {
+			var spanSearch = document.getElementById('result');
+			filterTab.push(element);
+			spanSearch.innerHTML = filterTab;
+		}
+	}
+});
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,22 +1,43 @@
 //clique bouton affiche
 let tab = [];
 
-const es6 = () => {
+//ajout d'un nom
+document.getElementById('addName').addEventListener('click', () => {
 	const inputVal = document.getElementById('nom').value;
+
 	//mettre en maj la première lettre des données de l'input
 	const majName = inputVal.charAt(0).toUpperCase();
 	const name = majName + inputVal.slice(1);
+
 	//Si champ vide "Bonjour Père Noël !"
-	if (inputVal === ""){
+	if (!name){
 		document.getElementById('message').innerHTML = "Bonjour Père Noël !";	
 	}
 	// Si input rempli "Bonjour {nom saisi} !"
 	else {
-		document.getElementById('message').innerHTML = `Bonjour ${inputVal}`;
+		document.getElementById('message').innerHTML = `Bonjour ${name}`;
 		//les données de l'input modifiées s'affichent en liste
 		document.getElementById('list').appendChild(document.createElement('li')).innerHTML = name;
 		//les données de l'input modifiées s'ajoutent au tableau
 		tab.push(name);
-		console.log(tab);
 	}		
-}
+});
+
+//cherche un nom
+document.getElementById('searchName').addEventListener('click', () => {
+	let filterTab = [];
+	const inputSearch = document.getElementById('search').value;
+	const searchLower = inputSearch.toLowerCase();
+	//comparer les données entrées dans l'input search avec le tableau
+	for(const i = 0; i < tab.length; i++){
+		const element = tab[i];
+		const elementLower = element.toLowerCase();
+		const filter = elementLower.indexOf(searchLower);
+		//affichage dans result des noms du tableau "commençant" par la chaîne de caractères saisie dans inputSearch
+		if(filter === 0){
+			const spanSearch = document.getElementById('result');
+			filterTab.push(element);
+			spanSearch.innerHTML = filterTab;
+		}
+	}
+});
